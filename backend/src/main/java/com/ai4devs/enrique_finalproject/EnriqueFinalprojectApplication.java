7f6d5f3d8c43f077764f8e18ab7a398b45bd0037
Enrique Rochina <enrique.rochina@gmail.com>
2024-09-14 00:05:17 +0200

    WIP

---
+++ backend/src/main/java/com/ai4devs/enrique_finalproject/EnriqueFinalprojectApplication.java
@@ -0,0 +1,13 @@
+package com.ai4devs.enrique_finalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnriqueFinalprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnriqueFinalprojectApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ai4devs/enrique_finalproject/controller/HelloControllerTest.java
@@ -0,0 +1,20 @@
+package com.ai4devs.enrique_finalproject.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private HelloController helloController;
+
+    @Test
+    public void testSayHello() throws Exception {
+
+    }
+}
