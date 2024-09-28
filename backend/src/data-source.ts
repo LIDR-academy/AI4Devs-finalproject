701da5f10abc090c57d0b2ef827c6e42b2cb26de
Edain92 <mpizarrocuevas@gmail.com>
2024-09-28 13:32:38 +0200

    fix: incluida cambiada la conexion para usar DATABASE_URL

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -8,11 +8,7 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT || "5432"),
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    url: process.env.DATABASE_URL,
     entities: [User, Activity, Trip],
     synchronize: process.env.NODE_ENV !== 'production',
     logging: false,
