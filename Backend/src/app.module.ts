653c271fc8f1fcc02927f90519915a7720989349
dvvid-g <giraldo.jose@correounivalle.edu.co>
2026-03-16 23:05:04 -0500

    fix bug

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -39,7 +39,7 @@ const serveFrontend =
       ? [
           ServeStaticModule.forRoot({
             rootPath: publicPath,
-            exclude: ['/api', '/api/(.*)'],
+            exclude: ['/api'],
           }),
         ]
       : []),
