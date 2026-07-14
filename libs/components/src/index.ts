8d6c3ff6f40ede320e7da41ec53968c549acea2f
Hernán Laura <hernan.laura@openenglish.com>
2026-07-14 12:15:29 -0300

    Update index.ts

--- libs/components/src/index.ts
+++ libs/components/src/index.ts
@@ -1,5 +1,7 @@
+// biome-ignore assist/source/organizeImports: <theme must stay first: registers StyleSheet.configure before any atom/molecule/organism module evaluates its StyleSheet.create call.>
+export * from './theme';
 export * from './atoms';
 export * from './molecules';
 export * from './organisms';
 export * from './templates';
-export * from './theme';
+
